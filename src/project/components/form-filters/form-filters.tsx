7e8a105aab1836cb009c275f58e249f5698a88ff
Nata <erbet@mail.ru>
2024-01-05 17:47:33 +0300

    fix form-filter

--- src/project/components/form-filters/form-filters.tsx
+++ src/project/components/form-filters/form-filters.tsx
@@ -3,80 +3,86 @@ import { TopicNamesMap, ComplicationMamesMap } from '../../const/const';
 import { TTopic, TComplication } from '../../types/types';
 
 
-type FormFilters ={
+type FormFilters = {
   onClickTopic: (filterTopic: TTopic) => void;
   activeFilterTopic: TTopic;
   activeFilterComplication: TComplication;
   onClickComplication: (filterComplication: TComplication) => void;
 };
 
-function FormFilters({ activeFilterTopic, activeFilterComplication, onClickTopic, onClickComplication }: FormFilters) {
+function FormFilters({
+  activeFilterTopic,
+  activeFilterComplication,
+  onClickTopic,
+  onClickComplication
+}: FormFilters) {
 
   const handleTopicFilterChange = (value: TTopic) => {
     onClickTopic(value);
   };
 
-  const handleComplicatioFilterChange = (value: TComplication) => {
+  const handleComplicationFilterChange = (value: TComplication) => {
     onClickComplication(value);
   };
 
 
-  return(
+  return (
     <form className="filter" action="#" method="get">
       <fieldset className="filter__section">
         <legend className="visually-hidden">Тематика</legend>
         <ul className="filter__list">
-          {(
-            Object.entries(TopicNamesMap) as[
-              TTopic,
-              (typeof TopicNamesMap)[TTopic]
-            ][]
-          ).map(([key, value]) => (
-            <li
-              className="filter__item"
-              key={key}
-            >
-              <input
-                type="radio"
-                name="type"
-                id={value}
-                checked = {value === activeFilterTopic}
-                onChange={() => handleTopicFilterChange(value)}
-              />
-              <label className="filter__label" htmlFor={key}>
-                <svg className="filter__icon" width={26} height={30} aria-hidden="true">
-                  <use xlinkHref={`#icon-${key}`} />
-                </svg>
-                <span className="filter__label-text">{value}</span>
-              </label>
-            </li>
-          ))}
-        </ul>
-      </fieldset>
-
-
-      <fieldset className="filter__section">
-        <legend className="visually-hidden">Сложность</legend>
-        <ul className="filter__list">
-          {(Object.entries(ComplicationMamesMap))
-            .map(([key, value]) =>(
+          {
+            Object.entries(TopicNamesMap).map(([key, value]) => (
               <li
                 className="filter__item"
                 key={key}
               >
                 <input
                   type="radio"
-                  name="level"
-                  id={value}
-                  checked={value === activeFilterComplication}
-                  onChange={() => handleComplicatioFilterChange(value)}
-
+                  name="type"
+                  id={key}
+                  checked = {key === activeFilterTopic}
+                  onChange={() => handleTopicFilterChange(key as TTopic)}
                 />
-                <label className="filter__label" htmlFor="any">
+                <label className="filter__label" htmlFor={key}>
+                  <svg
+                    className="filter__icon"
+                    width={26}
+                    height={30}
+                    aria-hidden="true"
+                  >
+                    <use xlinkHref={`#icon-${key}`} />
+                  </svg>
                   <span className="filter__label-text">{value}</span>
                 </label>
               </li>
-            ))}
+            ))
+          }
+        </ul>
+      </fieldset>
+
+      <fieldset className="filter__section">
+        <legend className="visually-hidden">Сложность</legend>
+        <ul className="filter__list">
+          {Object.entries(ComplicationMamesMap).map(([key, value]) =>(
+            <li
+              className="filter__item"
+              key={key}
+            >
+              <input
+                type="radio"
+                name="level"
+                id={key}
+                checked={key === activeFilterComplication}
+                onChange={() =>
+                  handleComplicationFilterChange(key as TComplication)q}
+
+              />
+              <label className="filter__label" htmlFor="any">
+                <span className="filter__label-text">{value}</span>
+              </label>
+            </li>
+          ))}
         </ul>
       </fieldset>
     </form>
--- src/project/pages/catalog/catalog.tsx
+++ src/project/pages/catalog/catalog.tsx
@@ -2,67 +2,39 @@ import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
 import { Card } from '../../components/card/card';
 import { FormFilters } from '../../components/form-filters/form-filters';
-import {TopicNamesMap, ComplicationMamesMap} from '../../const/const';
 import { TQuest, TTopic, TComplication } from '../../types/types';
-import { sortingByTopic, sortingByComplication } from '../../utils/filters';
+import { filterByTopic, filterByComplication } from '../../utils/filters';
 import { useState } from 'react';
 
-type CatalogProps ={
+type CatalogProps = {
   quests: TQuest[];
 }
 
-function Catalog({quests}: CatalogProps) {
-
+function Catalog({ quests }: CatalogProps) {
   const [activeFilterTopic, setFilterTopic] = useState<TTopic>('allQuests');
   const [activeFilterComplication, setFilterComplication] = useState<TComplication>('any');
 
-  const getSortingQuestsByTopic = (filter: TTopic) => {
-    switch(filter) {
-      case TopicNamesMap.allQuests:
-      default:
-        return sortingByTopic.allQuests(quests);
-      case TopicNamesMap.adventure:
-        return sortingByTopic.adventure(quests);
-      case TopicNamesMap.horror:
-        return sortingByTopic.horror(quests);
-      case TopicNamesMap.mystic:
-        return sortingByTopic.mystic(quests);
-      case TopicNamesMap.detective:
-        return sortingByTopic.detective(quests);
-      case TopicNamesMap['sci-fi']:
-        return sortingByTopic['sci-fi'](quests);
-    }
-  };
+  const getFilterQuestsByTopic = (filter: TTopic) =>
+    filterByTopic[filter](quests);
 
-  let sortedQuestsByTopic = getSortingQuestsByTopic(activeFilterTopic);
+  let filteredQuestsByTopic = getFilterQuestsByTopic(activeFilterTopic);
 
-  const getSortingQuestsByComplication = (filter: TComplication) => {
-    switch(filter) {
-      case ComplicationMamesMap.any:
-      default:
-        return sortingByComplication.any(sortedQuestsByTopic);
-      case ComplicationMamesMap.easy:
-        return sortingByComplication.easy(sortedQuestsByTopic);
-      case ComplicationMamesMap.hard:
-        return sortingByComplication.hard(sortedQuestsByTopic);
-      case ComplicationMamesMap.medium:
-        return sortingByComplication.medium(sortedQuestsByTopic);
-    }
-  };
+  const getFilteredQuestsByComplication = (filter: TComplication) =>
+    filterByComplication[filter](filteredQuestsByTopic);
 
-  let sortedQuestsByComplication = getSortingQuestsByComplication(activeFilterComplication);
+  let filteredQuestsByComplication = getFilteredQuestsByComplication(activeFilterComplication);
 
-  const handleSortTopicTypes = (filter: TTopic) => {
-    sortedQuestsByTopic = getSortingQuestsByTopic(filter);
+  const handleFilterTopic = (filter: TTopic) => {
+    filteredQuestsByTopic = getFilterQuestsByTopic(filter);
     setFilterTopic(filter);
   };
 
-  const handleSortComplicationTypes = (filter: TComplication) => {
-    sortedQuestsByComplication = getSortingQuestsByComplication(filter);
+  const handleFilterComplication = (filter: TComplication) => {
+    filteredQuestsByComplication = getFilteredQuestsByComplication(filter);
     setFilterComplication(filter);
   };
 
-  return(
+  return (
     <>
       <Header />
       <main className="page-content">
@@ -78,14 +50,14 @@ function Catalog({quests}: CatalogProps) {
           <div className="page-content__item">
             <FormFilters
               activeFilterTopic ={activeFilterTopic}
-              onClickTopic ={handleSortTopicTypes}
+              onClickTopic ={handleFilterTopic}
               activeFilterComplication={activeFilterComplication}
-              onClickComplication = {handleSortComplicationTypes}
+              onClickComplication = {handleFilterComplication}
             />
           </div>
           <h2 className="title visually-hidden">Выберите квест</h2>
           <div className="cards-grid">
-            {sortedQuestsByComplication.map((quest) => (
+            {filteredQuestsByComplication.map((quest) => (
               <Card
                 quest={quest}
                 key={quest.id}
